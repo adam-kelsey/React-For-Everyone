1d1da1577f9671e3f04569167d04a6ecb03eb03d
Adam Kelsey <adam.kelsey113@gmail.com>
2016-03-01 17:29:47 -0500

    iterate over array and pass each item into child component

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,7 @@ class Contact extends React.Component {
   render() {
     return (
       <li>
-        {this.props.contact.name} {this.props.contact.phone}</li>
+        {this.props.contact.name} {this.props.contact.phone}
       </li>
     )
   }
--- src/app.js
+++ src/app.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ContactsList from './ContactsList';
 
 let contacts = [{
+  id: 1,
   name: 'Adam',
   phone: '555 333 5555'
 }, {
+  id: 2,
   name: 'Person',
   phone: '111 111 1111'
 }, {
+  id: 3,
   name: 'Guuy',
   phone: '222 222 2222'
 }, {
+  id: 4,
   name: 'Girl',
   phone: '333 333 3333'
 }]
